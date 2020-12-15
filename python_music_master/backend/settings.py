275fe7b89baceb563d0cba595546dbd9ff9b86e3
Vishnu Narayanan S R <56758185+VishnuNarayananSR@users.noreply.github.com>
2020-12-16 01:08:03 +0530

    Update settings.py

--- python_music_master/backend/settings.py
+++ python_music_master/backend/settings.py
@@ -6,6 +6,6 @@ DURATION = t * SR #duration of audio signal
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #project path
 BATCH_SIZE = 32 #Training batch size
 SAVED_MODEL_PATH = BASE_DIR + '/Trained model/Conv1D' #path to save trained model
-MEDIA_DIR = os.path.abspath(__file__ + 3 *'/..') + '/media'    #path to media folder
+MEDIA_DIR = os.path.abspath(__file__ + 2 *'/..') + '/media'    #path to media folder
 PREDICTION_THRESHOLD=0.5    #How confident the model should be, to print a label as valid output
 LABELS_FILE = BASE_DIR+'/labelfile'
